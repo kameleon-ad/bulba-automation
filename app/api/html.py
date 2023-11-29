8ef17efa3bd7740256054cfc2f029bf0f1756512
kameleon-ad <alvindelos439@gmail.com>
2023-11-29 23:02:36 +0100

    Retreive HTML

--- app/api/html.py
+++ app/api/html.py
@@ -5,6 +5,16 @@ from app.models import Html
 html_api_blueprint = Blueprint('html-api', __name__)
 
 
+@html_api_blueprint.get("/problem")
+def retrieve_problems():
+    problems = Html \
+        .query \
+        .filter_by(problem=True) \
+        .with_entities(Html.task_id) \
+        .all()
+    return jsonify(problems)
+
+
 @html_api_blueprint.post("/problem")
 def problem_html_upload():
     return jsonify({"success": True})
