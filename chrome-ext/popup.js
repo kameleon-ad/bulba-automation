332252d3186b7b0a4a2a4b6a46011d3c0769f073
kameleon-ad <alvindelos439@outlook.com>
2023-11-29 22:09:08 +0000

    Extract task id and send

--- chrome-ext/popup.js
+++ chrome-ext/popup.js
@@ -2,15 +2,18 @@ document.getElementById('click-me').addEventListener('click', () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         chrome.scripting.executeScript({
             target: {tabId: tabs[0].id},
-            function: changeBackgroundColor
+            function: upload_problem,
         });
     });
 });
 
-function changeBackgroundColor() {
+function upload_problem() {
     const [instruction, prompt, response_a, response_b] = Array
         .from(document.getElementsByClassName("MuiPaper-root MuiPaper-elevation1 MuiPaper-rounded"))
         .map(element => element.innerHTML);
+    
+    const task_id_element = document.querySelector('div > strong + em');
+    const task_id = task_id_element.textContent.trim();
 
     fetch("http://localhost:5000/api/bulba/openai", {
         method: 'POST',
@@ -18,6 +21,7 @@ function changeBackgroundColor() {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: new URLSearchParams({
+            task_id,
             html_content: document.documentElement.innerHTML,
             instruction,
             prompt,
