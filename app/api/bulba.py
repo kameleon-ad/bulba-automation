495462ad08718627cfacf81b0c5bc67a95e3be75
kameleon-ad <alvindelos439@gmail.com>
2023-11-29 21:54:30 +0100

    Some minor changes

--- app/api/bulba.py
+++ app/api/bulba.py
@@ -9,7 +9,6 @@ bulba_api_blueprint = Blueprint('bulba-api', __name__)
 @bulba_api_blueprint.post("/openai")
 def bulba_openai():
     _, instruction, prompt, response_a, response_b = parse_bulba_request()
-    print(prompt)
     return jsonify({"success": True})
 
 
