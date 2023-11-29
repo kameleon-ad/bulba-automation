b77d629ef0a0a63d2eb979723d7bcf3f612181cc
kameleon-ad <alvindelos439@gmail.com>
2023-11-29 22:07:12 +0100

    Add HTML API

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify
 
 from .bulba import bulba_api_blueprint
+from .html import html_api_blueprint
 
 api_blueprint = Blueprint('api', __name__)
 api_blueprint.register_blueprint(bulba_api_blueprint, url_prefix='/bulba')
+api_blueprint.register_blueprint(html_api_blueprint, url_prefix="/html")
 
 
 @api_blueprint.get('/health')
---
+++ app/api/html.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify
+
+html_api_blueprint = Blueprint('html-api', __name__)
+
+
+@html_api_blueprint.post("/problem")
+def problem_html_upload():
+    return jsonify({"success": True})
+
+
+@html_api_blueprint.post("/feedback")
+def feedback_html_upload():
+    return jsonify({"success": True})
