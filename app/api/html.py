de4807de1d78c8772f4925ee1a5d3defe7d35e14
kameleon-ad <alvindelos439@gmail.com>
2023-11-30 04:39:35 +0100

    Midway of parsing the feedback

--- app/api/html.py
+++ app/api/html.py
@@ -11,7 +11,7 @@ from app.utils import parse_bulba_request, clear_scripts
 html_api_blueprint = Blueprint('html-api', __name__)
 
 
-@html_api_blueprint.get("/problem")
+@html_api_blueprint.get("/problems")
 def retrieve_problems():
     problems = Html \
         .query \
@@ -21,7 +21,7 @@ def retrieve_problems():
     return jsonify([task_id for task_id, in problems])
 
 
-@html_api_blueprint.post("/problem")
+@html_api_blueprint.post("/problems")
 def problem_html_upload():
     task_id, html_content, _, _, _, _ = parse_bulba_request()
     exists = SQL_DB.session.query(
@@ -33,7 +33,7 @@ def problem_html_upload():
     return jsonify({"success": True})
 
 
-@html_api_blueprint.get("/problem/<string:task_id>")
+@html_api_blueprint.get("/problems/<string:task_id>")
 def retrieve_problem(task_id: str):
     problem = Html \
         .query \
@@ -46,7 +46,17 @@ def retrieve_problem(task_id: str):
     return Response(clear_scripts(html_content))
 
 
-@html_api_blueprint.post("/feedback")
+@html_api_blueprint.get("/feedbacks")
+def retrieve_feedbacks():
+    problems = Html \
+        .query \
+        .filter_by(problem=False) \
+        .with_entities(Html.task_id) \
+        .all()
+    return jsonify([task_id for task_id, in problems])
+
+
+@html_api_blueprint.post("/feedbacks")
 def feedback_html_upload():
     task_id, html_content, _, _, _, _ = parse_bulba_request()
     return jsonify({"success": True})
--- chrome-ext/popup.js
+++ chrome-ext/popup.js
@@ -1,21 +1,57 @@
-document.getElementById('update-problem').addEventListener('click', () => {
+document.getElementById('upload-feedback').addEventListener('click', () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         chrome.scripting.executeScript({
             target: {tabId: tabs[0].id},
-            function: upload_problem
+            function: upload_feedback,
+        });
+    });
+});
+
+
+function upload_feedback() {
+    fetch('http://localhost:5000/api/html/feedback', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            task_id: '',
+            html_content: document.documentElement.innerHTML,
+            instruction: '',
+            prompt: '',
+            response_a: '',
+            response_b: '',
+        }),
+        redirect: 'follow'
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error', error);
+    });
+}
+
+
+document.getElementById('upload-problem').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.scripting.executeScript({
+            target: {tabId: tabs[0].id},
+            function: upload_problem,
         });
     });
 });
 
 function upload_problem() {
     const [instruction, prompt, response_a, response_b] = Array
-        .from(document.getElementsByClassName("MuiPaper-root MuiPaper-elevation1 MuiPaper-rounded"))
+        .from(document.getElementsByClassName('MuiPaper-root MuiPaper-elevation1 MuiPaper-rounded'))
         .map(element => element.innerHTML);
     
     const task_id_element = document.querySelector('div > strong + em');
     const task_id = task_id_element.textContent.trim();
 
-    fetch("http://localhost:5000/api/html/problem", {
+    fetch('http://localhost:5000/api/html/problems', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
