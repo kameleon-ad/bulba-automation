701bba5caaf6316155ea8cd796483320540239b9
kameleon-ad <alvindelos439@gmail.com>
2023-12-05 15:41:01 +0100

    Create v2 utils

--- app/api/bulba_v2/__init__.py
+++ app/api/bulba_v2/__init__.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, jsonify, request
 
+from app.utils import parse_html_to_md
+
 bulba_v2_api_blueprint = Blueprint('bulba_v2_api', __name__)
 
 
 @bulba_v2_api_blueprint.post('')
 def bulba_v2_determine():
+    prompt, response_a, response_b = [
+        parse_html_to_md(request.form.get(key))
+        for key in ['prompt', 'response_a', 'response_b']
+    ]
     return jsonify(request.form.keys())
