98c05b1ace3cb9173549ac0f2b5c7875074a8d6f
kameleon-ad <alvindelos439@gmail.com>
2023-12-11 10:44:36 +0100

    Used multithreading

--- app/api/bulba_v2/__init__.py
+++ app/api/bulba_v2/__init__.py
@@ -1,3 +1,6 @@
+import concurrent
+from concurrent.futures import ThreadPoolExecutor
+
 from flask import Blueprint, jsonify, request
 
 from app.utils import parse_html_to_md
@@ -18,10 +21,26 @@ def bulba_v2_determine():
         parse_html_to_md(request.form.get(key))
         for key in ['prompt', 'response_a', 'response_b']
     ]
-    return jsonify({
-        "category": code_related_and_category_and_complex(prompt),
-        "truthful_and_correct": truthful_and_correct(prompt, response_a, response_b),
-        "sxs": sxs(prompt, response_a, response_b),
-        **verbose_and_safe_and_harmless(prompt, response_a, response_b),
-        **overall_quality(prompt, response_a, response_b),
-    })
+
+    with ThreadPoolExecutor() as executor:
+        future_to_function = {
+            executor.submit(code_related_and_category_and_complex, prompt): "category",
+            executor.submit(truthful_and_correct, prompt, response_a, response_b): "truthful_and_correct",
+            executor.submit(sxs, prompt, response_a, response_b): "sxs",
+            executor.submit(verbose_and_safe_and_harmless, prompt, response_a, response_b): "verbose_and_safe_and_harmless",
+            executor.submit(overall_quality, prompt, response_a, response_b): "overall_quality"
+        }
+
+        results = {}
+        for future in concurrent.futures.as_completed(future_to_function):
+            func_name = future_to_function[future]
+            try:
+                data = future.result()
+                if isinstance(data, dict):
+                    results.update(data)
+                else:
+                    results[func_name] = data
+            except Exception as exc:
+                print(f'{func_name} generated an exception: {exc}')
+
+    return jsonify(results)
